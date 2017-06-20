58c614f18ab4e10ef82f43e0cb8a742dcf7271c4
Titus <titusdeng@gmail.com>
2017-06-20 18:53:25 -0500

    Fixed errors with nfc code

--- Bike Code/nfc_code.py
+++ Bike Code/nfc_code.py
@@ -11,31 +11,35 @@ import datetime
 import requests
 import urllib
 import httplib
+import json
 
 
 def connected(tag):
-    print (tag)
+    print ("Tag: " + str(tag))
     return False
 
 clf = nfc.ContactlessFrontend('usb')
+print "NFC Sensor Connected"
 
 while True:
-    #API_ENDPOINT = API_ENDPOINT = "digitalgym.cq4d8vjo7uoe.us-west-2.rds.amazonaws.com:3306"
-    # API_KEY = "ashu1234"
     API_ENDPOINT = "http://52.34.141.31:8000/bbb/process_tag"
 
-    Tag = clf.connect(rdwr={'on-connect': connected})
+    tag = clf.connect(rdwr={'on-connect': connected})
+
+    if (tag == False):
+        print "\nNFC Sensor Disconnected"
+        break
 
     # Extract the ID from the Tag object and convert it into an integer
-    RFID = int("0x" + str(Tag.identifier.encode("hex")), 16)
+    RFID = int("0x" + str(tag.identifier.encode("hex")), 16)
     data = {"RFID": RFID, "serialNumber": serial.getserial()}
-    try: #TODO: This try-except may be wrong
+    try:
         r = requests.post(url=API_ENDPOINT, data=data)
         # extracting response text
-        pastebin_url = r.text
-        print("the pastebin URL is: %s" % pastebin_url)
+        resp = json.loads(r.text)
+        print("Status: %s" % resp["status"])
     except requests.exceptions.RequestException as e:
-        print e
+        print "ERROR: " + str(e)
 
 
 
