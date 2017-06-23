93086538c62958959e13fb3baaa3df4a28372088
Hamza Nauman <hn9@rice.edu>
2017-06-23 15:46:50 -0500

    Changed bash file a bit

--- bike.sh
+++ bike.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script that runs the bike_code processes.
 
-sudo python ./nfc_code & sudo python ./rpm_sensor && killall -9 rpm_sensor
+sudo python ./Bike\ Code/nfc_code & sudo python ./Bike\ Code/rpm_sensor && killall -9 rpm_sensor
