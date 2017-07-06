4c7aadff5fd6b13bae9959d711f76e286f5cdf01
Hamza Nauman <hn9@rice.edu>
2017-07-06 00:41:13 -0500

    Created a new python file to check for connection with backend and send an email if connection fails. Also changed the script file accordingly.

---
+++ Bike Code/alert.py
@@ -0,0 +1,69 @@
+import smtplib
+from email.mime.text import MIMEText
+import time
+import json
+import serial
+import requests
+
+# API endpoint for testing connection with backend.
+API_TEST_CONNECTION = "http://52.34.141.31:8000/bbb/test_connection"
+
+me = "digital.gym.alert@gmail.com"            # Email address for sender
+target = "hn9@rice.edu"                       # Email address for recipient
+server = smtplib.SMTP("smtp.gmail.com", 587)  # Initiate Email Server
+serial_num = serial.getserial()               # Serial Number of bike this code is running on
+# serial_num = "12345"
+
+global error  # Global variable indicating whether there is an error currently
+
+
+def main():
+    global error
+
+    error = False  # No error initially
+
+    while True:
+        time.sleep(10)  # Recheck connection with server every 10 seconds
+        try:
+            resp = requests.get(API_TEST_CONNECTION)  # Test Connection
+            print json.loads(resp.text)["status"]  # Should be "success" if connection established
+
+            # If there was previously an error, erase it and send an email signifying that
+            # the connection with the server has been restored.
+            if error:
+                error = False
+                send_email("Restored")
+
+        except requests.exceptions.RequestException as e:
+            print e
+            # If there wasn't previously an error, create it and send an email signifying that
+            # the connection with the server has failed.
+            if not error:
+                error = True
+                send_email("Failed")
+
+
+def send_email(event):
+    """
+    Sends an email to the target email specifying the time and date of "event".
+    """
+    now_date = time.strftime("%x")  # Current Date
+    now_time = time.strftime("%X")  # Current Time
+
+    # Main text of email.
+    data = event + " Date: " + now_date + "\n" + \
+        event + " Time: " + now_time + "\n"
+
+    msg = MIMEText(data)
+
+    msg["Subject"] = "Connection " + event + " on Bike Serial #" + serial_num
+    msg["From"] = me
+    msg["To"] = target
+
+    server.sendmail(me, target, msg.as_string())
+
+
+if __name__ == "__main__":
+    server.starttls()
+    server.login(me, "ashu1234")  # Login into sender email address
+    main()
--- bike.sh
+++ bike.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # A script that runs the bike_code processes.
 
-sudo python ./Bike\ Code/nfc_code.py & sudo python ./Bike\ Code/rpm_sensor.py && pkill nfc_code
+sudo python ./Bike\ Code/nfc_code.py &
+sudo python ./Bike\ Code/rpm_sensor.py &
+sudo python ./Bike\ Code/alert.py &&
+pkill rpm_sensor
+pkill nfc_code
