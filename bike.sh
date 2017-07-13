236024f5b219b6c94215fdd7a120ddb8a0157136
Hamza Nauman <hn9@rice.edu>
2017-07-12 20:16:38 -0500

    Fixed IOError 19

--- bike.sh
+++ bike.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # A script that runs the bike_code processes.
 
+sudo killall python >nul 2>&1
 sudo python ./bike_code/alert.py &
 sudo python ./bike_code/nfc_code.py &
 sudo python ./bike_code/rpm_sensor.py &&
