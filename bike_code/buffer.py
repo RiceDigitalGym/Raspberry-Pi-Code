48204a669342d153a2369ccb1a04bc8d0cfda953
mario0man <mario0man@gmail.com>
2017-10-26 03:08:33 -0500

    Begin work on Buffer mechanism

--- bike_code/buffer.py
+++ bike_code/buffer.py
@@ -1,8 +1,53 @@
-from __future__ import print_function
+'''
+Tries to send RPM info from the bike directly to online if the POST 
+request is successful (i.e. 500). If an error (i.e. 404) is received, 
+send to local buffer while simultaneously attempting to offload 
+buffered data to online. 
 
+Alternatively, constantly send the information to the local buffer 
+and simultaneously attempt to send to server regardless of 500/404. 
+
+'''
+
+from multiprocessing import Process
+from __future__ import print_function
 import os
 import shelve
+import requests 
 
+# Define the API endpoint:
+API_ENDPOINT = "http://52.34.141.31:8000/bbb/bike"
+API_START_WORKOUT = "http://52.34.141.31:8000/bbb/start_workout"
+API_END_WORKOUT = "http://52.34.141.31:8000/bbb/end_workout"
+
+
+def process_data(data, shelf):
+	'''
+	Sends the given data to a given LocalShelve object while attempting to send to server. 
+	'''
+	def send_rpm(post_data):
+		r = requests.post(url=API_ENDPOINT, data=post_data)
+		return r.status_code
+	
+	def send_first_to_server(shelf):
+		'''
+		Tries to send the first entry in the LocalShelve to server. 
+		If sending is successful, then remove that entry from the LocalShelve. 
+		'''
+		buffer = shelf.open(shelf)
+		# extract each column of data from buffer - serials, statuses, rpms, and rfids 
+		serials = buffer['serialNumber']
+		statuses = buffer['status']
+		rpms = buffer['rpm'] 
+		rfids = buffer['RFID']
+		# format the first entry as POST data 
+		first_entry = {'serialNumber': serials[1], 'statuses': statuses[1], 
+			'rpm': rpms[1], 'RFID': rfids[1]}
+		# send the first entry. if the response is ok, then remove the entry from the buffer. 
+		# do this by placing back all entries minus the first into the shelf. 
+		if send_rpm(first_entry) == requests.codes.ok: 
+			
+		
 
 class LocalShelve(object):
     def __init__(self, filename):
