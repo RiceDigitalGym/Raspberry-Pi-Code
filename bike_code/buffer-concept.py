a3b8879ec8d07b6f3d4203fd4ce8916e46213e40
Sunil <mario0man@gmail.com>
2018-02-17 03:59:11 -0600

    buffer concept

---
+++ bike_code/buffer-concept.py
@@ -0,0 +1,40 @@
+'''
+Mockup of multiprocessing structure for local buffer system
+
+TODO: 
+    - put in place the actual buffer instead of 'count' method
+    - reroute pinging to the actual server backend instead of google
+    
+'''
+
+import subprocess
+import multiprocessing 
+import time
+
+def ping(): 
+    while(1):
+        c1 = 'ping google.com -n 1'.split()
+        out = subprocess.check_output(c1, shell=True, encoding='utf-8') 
+        out = out.split('\n')
+        target = '' 
+        for line in out: 
+            if 'Reply' in line: 
+                target = line 
+        print(target)
+
+def count(): 
+    i = 0
+    while(1): 
+        time.sleep(0.1)
+        print(i)
+        i += 1
+        
+def main(): 
+    f1 = multiprocessing.Process(target=ping)
+    f2 = multiprocessing.Process(target=count) 
+    
+    f1.start()
+    f2.start()
+    
+if __name__ == "__main__":
+    main() 
